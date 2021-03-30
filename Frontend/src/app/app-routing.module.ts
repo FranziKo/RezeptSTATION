25f1fbf16793c27709889ff46e3896c7a070017c
FranziKo <franzi.kopp@gmx.de>
2021-03-30 13:55:30 +0200

    Routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {FriendsComponent} from "./friends/friends.component";
+import {RecipesComponent} from "./recipes/recipes.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'friends', component: FriendsComponent},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material-module";
+import { HomepageComponent } from './homepage/homepage.component';
+import { FriendsComponent } from './friends/friends.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FriendsComponent,
+    RecipesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
